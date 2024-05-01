90195c1560292b5f7bde66d13adb6dee96e44626
DiogoRibeiro7 <diogo_dj@hotmail.com>
2024-05-01 08:56:56 +0100

    chore: concate spread sheets

---
+++ combine_EIS.R
@@ -0,0 +1,12 @@
+library(readxl)
+library(dplyr)
+library(writexl)
+
+data1 <- read_excel("EIS_DATA.xlsx", sheet = "EIS-INN")
+data2 <- read_excel("EIS_DATA.xlsx", sheet = "EIS_STR")
+
+combined_data <- bind_rows(data1, data2)
+
+write_xlsx(combined_data, "combined_data.xlsx")
+
+View(combined_data)
